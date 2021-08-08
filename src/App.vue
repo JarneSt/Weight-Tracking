f9cafa7fb602445613f0c574930703bdc3e03b91
Jarne Staal <68494791+JarneSt@users.noreply.github.com>
2021-08-08 18:00:04 +0200

    Design updates and toFixed(2) bug fixed. Reset implemented if starting weight was incorrect

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="nav">
-    <router-link to="/">Weight Tracker</router-link>
+    <h1>Weight Tracker</h1>
   </div>
   <router-view/>
 </template>
 
+<style scoped>
+#nav h1 {
+  color: white;
+}
+</style>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Poppins Light",serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -16,11 +21,13 @@
 
 #nav {
   padding: 30px;
+  background: #2fb5ee;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+
 }
 
 #nav a.router-link-exact-active {
--- src/components/WeightTracking.vue
+++ src/components/WeightTracking.vue
@@ -1,34 +1,31 @@
 <template>
 <div class="mt-5">
-  <input placeholder="weight" class="text-align-center" v-model="weightInput">
+  <input placeholder="weight" type="number" class="text-align-center form-control" v-model="weightInput">
+  <input class="form-control " v-if="startingWeightSet === false" type="button" @click="setWeight" value="Set the first weight">
   <input class="form-control" v-if="startingWeightSet === true" type="button" @click="weightCalculation" value="Calculate weight difference">
-  <input class="form-control" v-if="startingWeightSet === false" type="button" @click="setWeight" value="Set the first weight">
+  <input type="button" class="form-control" @click="resetAll" value="Reset">
 
-
-
-  <div class="mt-5">
-    <h1 v-if="lastWeight">Last weight {{lastWeight}}</h1>
-    <h1 v-if="weightInput">Current weight {{weightInput}}</h1>
-
-    <div v-if="weightStatus === 0 && startingWeightSet === true && lastWeight !== 0" class="mt-5">
-      <p>You have lost </p>
-      <p id="weightStatus0">{{weightDifferenceFixed}} kg</p>
-      <iframe src="https://giphy.com/embed/d2Z9QYzA2aidiWn6" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/snl-thumbs-up-awesome-d2Z9QYzA2aidiWn6"></a></p>    </div>
-
-  </div>
-
+  <div>
+    <div class="mt-5">
+      <h1 v-if="lastWeight">Last weight {{lastWeight}}</h1>
+      <h1 v-if="weightInput">Current weight {{weightInput}}</h1>
+      <div v-if="weightStatus === 0 && startingWeightSet === true && lastWeight !== 0" class="mt-5">
+        <p>You have lost </p>
+        <p id="weightStatus0">{{weightDifferenceFixed}} kg</p>
+        <iframe src="https://giphy.com/embed/d2Z9QYzA2aidiWn6" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/snl-thumbs-up-awesome-d2Z9QYzA2aidiWn6"></a></p>    </div>
+    </div>
     <div v-if="weightStatus === 1" class="mt-5">
       <p>Weight has not changed and is still</p>
       <p id="weightStatus1">{{weightDifferenceFixed}} kg</p>
       <iframe src="https://giphy.com/embed/opzWPcGA4zRVQJxHzG" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/kimsconvenience-opzWPcGA4zRVQJxHzG"></a></p>
     </div>
-
-
     <div v-if="weightStatus === 2" class="mt-5">
       <p>You have gained</p>
       <p id="weightStatus2">{{weightDifferenceFixed}} kg</p>
-      <iframe allow="fullscreen" frameBorder="0" height="480" src="https://giphy.com/embed/CvF6vRAf7QUgCvQ9KK/video"></iframe>
+      <div><iframe allow="fullscreen" frameBorder="0" height="270" src="https://giphy.com/embed/xSGJkRpfh7bPtdBkfj/video"></iframe></div>
     </div>
+  </div>
+
 </div>
 </template>
 
@@ -50,7 +47,16 @@ export default {
       return localStorage.getItem('lastWeight');
     },
     weightDifferenceFixed(){
-      return this.weightDifference.toFixed(2);
+      if (this.weightDifference > 0){
+        return this.weightDifference.toFixed(2);
+      }
+      else {
+        return this.weightDifference;
+      }
+    },
+    resetAll(){
+      localStorage.removeItem('lastWeight');
+      window.location.reload();
     }
   },
   mounted(){
@@ -72,43 +78,54 @@ export default {
     weightCalculation(){
       let lastWeight = localStorage.getItem('lastWeight');
       this.lastWeight = lastWeight;
-      if (lastWeight){
-        if (lastWeight > this.weightInput){
-          this.weightDifference = lastWeight - this.weightInput;
-          this.weightStatus = 0;
-        }
-        else if (lastWeight === this.weightInput){
-          this.weightDifference = this.weightInput;
-          this.weightStatus = 1;
-        }
-        else if (lastWeight < this.weightInput) {
-          this.weightDifference = this.weightInput - lastWeight ;
-          this.weightStatus = 2;
+      if (this.weightInput > 0){
+        if (lastWeight){
+          if (lastWeight > this.weightInput){
+            this.weightDifference = lastWeight - this.weightInput;
+            this.weightStatus = 0;
+          }
+          else if (lastWeight === this.weightInput){
+            this.weightDifference = this.weightInput;
+            this.weightStatus = 1;
+          }
+          else if (lastWeight < this.weightInput) {
+            this.weightDifference = this.weightInput - lastWeight ;
+            this.weightStatus = 2;
+          }
+          else {
+            this.weightStatus = null
+          }
+
+          localStorage.setItem('lastWeight',this.weightInput);
         }
         else {
-          this.weightStatus = null
+          localStorage.setItem('lastWeight',this.weightInput);
         }
-
-        localStorage.setItem('lastWeight',this.weightInput);
-      }
-      else {
-        localStorage.setItem('lastWeight',this.weightInput);
+      }else if (this.weightInput <= 0) {
+        alert('Please use a value above 0')
       }
 
       this.$forceUpdate;
     },
     setWeight(){
-      localStorage.setItem('lastWeight',this.weightInput);
-      let lstWeight = localStorage.getItem('lastWeight');
-      this.lastWeight = lstWeight;
-      this.$forceUpdate;
-      this.startingWeightSet = true;
+      if (this.weightInput > 0){
+        localStorage.setItem('lastWeight',this.weightInput);
+        let lstWeight = localStorage.getItem('lastWeight');
+        this.lastWeight = lstWeight;
+        this.$forceUpdate;
+        this.startingWeightSet = true;
+      }
     }
   },
 }
 </script>
 
 <style scoped>
+input[type=button]{
+  background: #2fb5ee;
+  color: white;
+}
+
 #weightStatus0{
   color: green;
 }
